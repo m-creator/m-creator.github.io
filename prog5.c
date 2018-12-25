40efc8d8c1d28e57ca270231a7b4b2b3342bd6b5
m-creator <mcreator302@gmail.com>
2018-12-25 22:07:44 +0900

    prog5test

---
+++ prog5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a=0,b=0;
+    printf("a = ");
+    scanf("%d",&a);
+    printf("a = ");
+    scanf("%d",&b);
+
+    printf("òa : %d\n",a+b);
+    printf("ç∑ : %d\n",a-b);
+    printf("êœ : %d\n",a*b);
+    printf("è§ : %d\n",a/b);
+
+
+}
